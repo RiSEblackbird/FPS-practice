0a1ea7c5e7d21321126f1dc6d965899d47ca5602
TAISHI SENOH <mokut2017@gmail.com>
2019-10-13 13:59:07 +0900

    ADD. Hello tutrial scripts
    玉転がしチュートリアルに同じ

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public UnityEngine.UI.Text scoreLabel;
+    public GameObject winnerLabelObject;
+    public GameObject RetryButton;
+
+    public void Update ()
+    {
+        int count = GameObject.FindGameObjectsWithTag ("Enemy").Length;
+        scoreLabel.text = count.ToString ();
+
+        if (count == 0) {
+            // オブジェクトをアクティブにする
+            winnerLabelObject.SetActive(true);
+        }
+    }
+
+    public void OnRetry ()
+    {
+        SceneManager.LoadScene(
+            SceneManager.GetActiveScene().name);
+    }
+}
