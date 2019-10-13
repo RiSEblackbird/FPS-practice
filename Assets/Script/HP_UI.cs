7aaecb66f719f65ddbcd72e0588214dc23cb2b26
TAISHI SENOH <mokut2017@gmail.com>
2019-10-13 14:00:07 +0900

    ADD. HP UI (frozen)
    保留

---
+++ Assets/Script/HP_UI.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class HP_UI : MonoBehaviour
+{
+   
+    void Update()
+    {
+        transform.rotation = Camera.main.transform.rotation; // MainCameraに対面する。
+    }
+}
