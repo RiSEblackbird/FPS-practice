1d1cd989429bd9301fdbcdf4cff1a2e389e8867a
TAISHI SENOH <mokut2017@gmail.com>
2019-10-13 13:54:31 +0900

    ADD. HP and Damage for enemy object

---
+++ Assets/Script/DestroyObject.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour
+{
+    public int damage;
+    private GameObject enemy;
+    private HP hp;
+       
+    void Start()
+    {
+        enemy = GameObject.FindWithTag("Enemy");
+        hp = enemy.GetComponent<HP>();
+    }
+
+    /* この場合はこりだーのトリガーにチェック！
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Shell"))
+        {
+            hp.Damage(damage);
+
+            Destroy(other.gameObject);
+        }
+    }
+    */
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Shell"))
+        {
+            hp.Damage(damage);
+
+            Destroy(other.gameObject);
+            
+        }
+    }
+}
---
+++ Assets/Script/HP.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HP : MonoBehaviour
+{
+    public int hitPoint = 100;
+
+    public GameObject Explosion;
+
+    void Update()
+    {
+        if (hitPoint <= 0)
+        {
+            Destroy(gameObject);
+
+            Explosion.SetActive(true);
+
+            Instantiate(Explosion, Explosion.transform.position, Quaternion.identity);
+        }
+    }
+
+    public void Damage(int damage)
+    {
+        hitPoint -= damage;
+
+    }
+}
